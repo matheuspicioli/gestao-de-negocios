6f518f8293873d4ded869e685ecc4d698cf167c6
Matheus Picioli <matheus.picioli98@gmail.com>
2019-08-31 23:02:12 -0300

    Partials itens p/ todos os módulos

---
+++ resources/views/partials/back-button.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-xs-12">
+        <a href="{{ route($url) }}" class="btn btn-default">
+            <i class="fa fa-arrow-left"></i>
+            Voltar
+        </a>
+    </div>
+</div>
+
+<div class="row">
+    &nbsp;
+</div>
