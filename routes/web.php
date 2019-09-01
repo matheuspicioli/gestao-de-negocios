6477d5d16eb85c907e93f6d151de7f21194fc50c
Matheus Picioli <matheus.picioli98@gmail.com>
2019-08-31 23:01:32 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('home');
+    Route::get('/', 'HomeController@index');
+    Route::get('/home', 'HomeController@index')->name('home');
+    
+    Route::prefix('produtos')->group(function () {
+        Route::get('/', 'ProdutosController@index')->name('produtos.listar');
+        Route::get('criar', 'ProdutosController@create')->name('produtos.criar');
+        Route::post('criar', 'ProdutosController@store')->name('produtos.salvar');
+        Route::get('{id}/editar', 'ProdutosController@edit')->name('produtos.editar');
+        Route::put('{id}/editar', 'ProdutosController@update')->name('produtos.atualizar');
+        Route::delete('{id}/deletar', 'ProdutosController@destroy')->name('produtos.deletar');
     });
     
-    Route::get('/home', function () {
-        return view('home');
+    Route::prefix('servicos')->group(function () {
+        Route::get('/', 'ServicosController@index')->name('servicos.listar');
+        Route::get('criar', 'ServicosController@create')->name('servicos.criar');
+        Route::post('criar', 'ServicosController@store')->name('servicos.salvar');
+        Route::get('{id}/editar', 'ServicosController@edit')->name('servicos.editar');
+        Route::put('{id}/editar', 'ServicosController@update')->name('servicos.atualizar');
+        Route::delete('{id}/deletar', 'ServicosController@destroy')->name('servicos.deletar');
+    });
+    
+    Route::prefix('lancamentos')->group(function () {
+        Route::get('/', 'LancamentosController@index')->name('lancamentos.listar');
     });
 });
