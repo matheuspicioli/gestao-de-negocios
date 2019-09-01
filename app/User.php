abb48ade6bc2add5db9b5802cca6908323ea28ae
Matheus Picioli <matheus.picioli98@gmail.com>
2019-08-31 23:03:46 -0300

    Relacionamento com lançamentos

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Lancamento;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function lancamentos ()
+    {
+        return $this->hasMany(Lancamento::class, 'produto_id', 'id');
+    }
 }
